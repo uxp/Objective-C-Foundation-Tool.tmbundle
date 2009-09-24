7c307a5f0c12c263ccd9810f29e5e3fe6107ce34
HPLogsdon <uxp@bsdeviant.org>
2009-09-23 23:59:43 -0600

    first commit

---
+++ Templates/Standard main().tmTemplate/main_in.m
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ${TM_NEW_FILE_BASENAME}.m
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  ${TM_DATE}
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  ${TM_FULLNAME}
+ *   Organization:  ${TM_ORGANIZATION_NAME}
+ *
+ * =====================================================================================
+ */
+
+#include <Foundation/Foundation.h>
+
+int main( int argc, const char * argv[] ){
+	
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	
+	// Code Goes Here.
+	NSLog(@"Hello World!");
+	
+	[pool release];
+	return 0;
+}
